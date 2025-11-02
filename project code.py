0fb93921548a458f9221bdd332895f271db8ee2b
xx xxxx <xx.xxxx@mail.com>
2025-11-02 20:51:55 +0100

    commit 1

--- project code.py
+++ project code.py
@@ -1,1 +1,13 @@
-
+# Persönlicher Budget Tracker
+#
+#
+#
+#
+#
+def add_income(data):
+    """Einnahme hinzufügen"""
+    date = input("Datum (TT.MM.JJJJ, leer = heute): ")
+    category = input("Kategorie (z.B. Lohn, Nebenjob): ")
+    amount = float(input("Betrag: "))
+    data.append({"Datum": date, "Typ": "Einnahme", "Kategorie": category, "Betrag": amount})
+    print("✅ Einnahme gespeichert.\n")
