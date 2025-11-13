c2548c1fb2f882cb91c488976900a43056d86fbe
3L3ON0R4 <eleonora.schoepfer@students.fhnw.ch>
2025-11-13 12:32:40 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    """
+    Hauptfunktion:
+    - Login ausführen
+    - Budgetdaten laden
+    - Hauptmenü in Schleife anzeigen
+    """
+  print("Willkommen beim Persönlichen Budget-Tracker!")
