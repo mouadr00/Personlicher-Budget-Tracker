f4357ba7d64755cefe602073a93314a02871b7ce
Mouad Rahali <rahali.mouad00@gmail.com>
2025-11-25 17:36:25 +0100

    last

--- project code.py
+++ project code.py
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
+# Persönlicher Budget Tracker
+#
+#
+#
+#
+#
+def add_income(data):
+    """Einnahme hinzufügen"""
+    date = input("Datum (TT.MM.JJJJ, leer = heute): ")
+    category = input("Kategorie (z.B. Lohn, Nebenjob): ")
+    amount = float(input("Betrag: "))
+    data.append({"Datum": date, "Typ": "Einnahme", "Kategorie": category, "Betrag": amount})
+    print("✅ Einnahme gespeichert.\n")
+=======
 # Persönlicher Budget Tracker
 #
 #
@@ -13,3 +28,11 @@ def add_income(data):
     print("Einnahme gespeichert.\n")
     
     
+>>>>>>> 5376cd57e9534407228cbfbba01fa22c84809dcb
+
+<<<<<<< HEAD
+
+
+
+=======
+>>>>>>> 5376cd57e9534407228cbfbba01fa22c84809dcb
