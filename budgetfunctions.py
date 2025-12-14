ff958a8bc320c09428c8b49150a97b30f39c9f8d
Mouad Rahali <rahali.mouad00@gmail.com>
2025-12-14 19:59:05 +0100

    Update budgetfunctions.py

--- budgetfunctions.py
+++ budgetfunctions.py
@@ -18,8 +18,8 @@ def add_income(data, allowed_categories):
     amount = input_amount("Betrag (+): ")
 
     # Monat/Jahr aus Datum extrahieren
-        dt = datetime.strptime(date, "%d.%m.%Y")
-        month, year = dt.month, dt.year
+    dt = datetime.strptime(date, "%d.%m.%Y")
+    month, year = dt.month, dt.year
 
 
     # Daten für ensprechenden Monat laden
