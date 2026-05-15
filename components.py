9207ec26f580fea9fcf9cb3dc7d50859adff74bd
3L3ON0R4 <eleonora.schoepfer@students.fhnw.ch>
2026-05-15 15:55:44 +0200

    Update components.py

--- components.py
+++ components.py
@@ -3,17 +3,11 @@ from nicegui import ui
 
 def sidebar():
 
-    with ui.column().classes(
-        'bg-blue-900 text-white h-screen w-64 p-4'
-    ):
+    with ui.column().classes('bg-blue-900 text-white h-screen w-64 p-4'):
         
-        ui.label('Budget Tracker').classes(
-            'text-2xl font-bold mb-8'
-            )
+        ui.label('Budget Tracker').classes('text-2xl font-bold mb-8')
         
-        ui.link('Dashboard').classes(
-            'text-white text-lg mb-3'
-        )
+        ui.link('Dashboard', '/').classes('text-white text-lg mb-3')
 
         ui.link('+ Einnahme hinzufügen, '/income').classes(
                 'text-white text-lg mb-3'
@@ -28,27 +22,18 @@ def sidebar():
 
         ui.link('Kategorien, '/categories').classes(
                 'text-white text-lg mb-3'
-
         )
 
 def page_layout(title: str):
 
     with ui.column().classes('w-full p-8'):
         
-        ui.label(title).classes(
-            'text-3xl font-bold text-blue-500 text-Helvetica mb-6'
-        )
+        ui.label(title).classes('text-3xl font-bold text-blue-500 text-Helvetica mb-6')
 
 def summary_card(title: str, value: str, color: str):):
 
-    with ui.card().classes(
-        f'p-6 w-64 shadow-lg rounded-xl bg-{color}-100'
-    ):
+    with ui.card().classes(f'p-6 w-64 shadow-lg rounded-xl bg-{color}-100'):
 
-        ui.label(title).classes(
-            'text-lg font-semibold text-gray-700 mb-2'
-        )
+        ui.label(title).classes('text-lg font-semibold text-gray-700 mb-2')
 
-        ui.label(value).classes(
-            'text-2xl font-bold text-gray-900'
-        )
+        ui.label(value).classes('text-2xl font-bold text-gray-900')
